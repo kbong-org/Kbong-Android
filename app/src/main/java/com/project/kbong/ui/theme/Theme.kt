1254bd1b57564228570738458a6c9ca4cd2b0311
yeomjiseok <ygs01033@naver.com>
2025-01-27 22:45:10 +0900

    feat: 컬러 추가

--- app/src/main/java/com/project/kbong/ui/theme/Theme.kt
+++ app/src/main/java/com/project/kbong/ui/theme/Theme.kt
@@ -11,16 +11,15 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
+/*private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
     tertiary = Pink80
-)
+)*/
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = KBongPrimary,
+    background = KBongBackground,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -35,19 +34,13 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun KbongAndroidTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+   // darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-        val context = LocalContext.current
-        if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-      }
-      darkTheme -> DarkColorScheme
-      else -> LightColorScheme
-    }
+    val colorScheme = LightColorScheme
+
 
     MaterialTheme(
       colorScheme = colorScheme,
