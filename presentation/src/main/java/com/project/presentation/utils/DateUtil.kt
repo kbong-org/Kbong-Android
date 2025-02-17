9eafb2a26d16a3585221ebc29db01333ae150e46
Ji Seok <38849158+JiSeokYeom@users.noreply.github.com>
2025-02-17 17:16:22 +0900

    feat: 홈 직관기록 일별 직관 헤더 컴포넌트 추가

--- presentation/src/main/java/com/project/presentation/utils/DateUtil.kt
+++ presentation/src/main/java/com/project/presentation/utils/DateUtil.kt
@@ -2,6 +2,7 @@ package com.project.presentation.utils
 
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
+import java.util.Locale
 
 private val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
 
@@ -16,4 +17,13 @@ fun String.stringToLocalDate(): LocalDate {
         LocalDate.now()
     else
         LocalDate.parse(this, formatter)
+}
+
+// ex) LocalDate -> 3월 16일 일요일
+fun LocalDate.formatLocalDate(): String {
+    val koreanLocale = Locale("ko", "KR")
+
+    val formatter = DateTimeFormatter.ofPattern("M월 d일 EEEE", koreanLocale)
+
+    return this.format(formatter)
 }
