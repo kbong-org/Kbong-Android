8d2c1616ec5e6046352195354067f5d6961dd8fa
yeomjiseok <ygs01033@naver.com>
2025-03-24 22:52:58 +0900

    feat: 구단별 필요한 데이터 추가

--- presentation/src/main/java/com/project/presentation/my/MyTeamType.kt
+++ presentation/src/main/java/com/project/presentation/my/MyTeamType.kt
@@ -2,7 +2,18 @@ package com.project.presentation.my
 
 import android.graphics.drawable.Drawable
 import androidx.compose.ui.graphics.Color
+import com.project.kbong.designsystem.theme.KBongPrimary
+import com.project.kbong.designsystem.theme.KBongTeamBears
+import com.project.kbong.designsystem.theme.KBongTeamEagles
+import com.project.kbong.designsystem.theme.KBongTeamGiants
+import com.project.kbong.designsystem.theme.KBongTeamGray10
+import com.project.kbong.designsystem.theme.KBongTeamHeroes
+import com.project.kbong.designsystem.theme.KBongTeamLions
+import com.project.kbong.designsystem.theme.KBongTeamNc
+import com.project.kbong.designsystem.theme.KBongTeamSsg
 import com.project.kbong.designsystem.theme.KBongTeamTigers
+import com.project.kbong.designsystem.theme.KBongTeamTwins
+import com.project.kbong.designsystem.theme.myPageBackgroundBlue
 import com.project.kbong.designsystem.theme.myPageBackgroundRed
 
 enum class MyTeamType(
@@ -12,28 +23,98 @@ enum class MyTeamType(
     val isBlueStadium: Boolean = true,
     val lottieFile: Drawable? = null,
 ) {
-    /* "KIA 타이거즈" to "KIA",
-     "두산 베어스" to "DOOSAN",
-     "롯데 자이언츠" to "LOTTE",
-     "삼성 라이온즈" to "SAMSUNG",
-     "SSG 랜더스" to "SSG",
-     "NC 다이노스" to "NC",
-     "LG 트윈스" to "LG",
-     "키움 히어로즈" to "KIWOOM",
-     "KT 위즈" to "KT",
-     "한화 이글즈" to "HANHWA",
-     "모두 응원해요" to "NONE"*/
+    DOOSAN(
+        teamName = "두산 베어스",
+        isBlueStadium = true,
+        backgroundColor = myPageBackgroundBlue,
+        teamTagBackgroundColor = KBongTeamBears,
+        lottieFile = null
+    ),
+
+    LOTTE(
+        teamName = "롯데 자이언츠",
+        isBlueStadium = true,
+        backgroundColor = myPageBackgroundBlue,
+        teamTagBackgroundColor = KBongTeamGiants,
+        lottieFile = null
+    ),
+
+    SAMSUNG(
+        teamName = "삼성 라이온즈",
+        isBlueStadium = true,
+        backgroundColor = myPageBackgroundBlue,
+        teamTagBackgroundColor = KBongTeamLions,
+        lottieFile = null
+    ),
+
+    SSG(
+        teamName = "SSG 랜더스",
+        isBlueStadium = false,
+        backgroundColor = myPageBackgroundRed,
+        teamTagBackgroundColor = KBongTeamSsg,
+        lottieFile = null
+    ),
+
+    NC(
+        teamName = "NC 다이노스",
+        isBlueStadium = true,
+        backgroundColor = myPageBackgroundBlue,
+        teamTagBackgroundColor = KBongTeamNc,
+        lottieFile = null
+    ),
+
+    LG(
+        teamName = "LG 트윈스",
+        isBlueStadium = false,
+        backgroundColor = myPageBackgroundRed,
+        teamTagBackgroundColor = KBongTeamTwins,
+        lottieFile = null
+    ),
+
+    KIWOOM(
+        teamName = "키움 히어로즈",
+        isBlueStadium = false,
+        backgroundColor = myPageBackgroundRed,
+        teamTagBackgroundColor = KBongTeamHeroes,
+        lottieFile = null
+    ),
+
+    KT(
+        teamName = "KT 위즈",
+        isBlueStadium = true,
+        backgroundColor = myPageBackgroundBlue,
+        teamTagBackgroundColor = KBongTeamGray10,
+        lottieFile = null
+    ),
+
+    HANHWA(
+        teamName = "한화 이글즈",
+        isBlueStadium = false,
+        backgroundColor = myPageBackgroundRed,
+        teamTagBackgroundColor = KBongTeamEagles,
+        lottieFile = null
+    ),
+
+
     KIA(
         teamName = "KIA 타이거즈",
         isBlueStadium = false,
         backgroundColor = myPageBackgroundRed,
         teamTagBackgroundColor = KBongTeamTigers,
         lottieFile = null
+    ),
+
+    NONE(
+        teamName = "모두 응원해요",
+        isBlueStadium = true,
+        backgroundColor = myPageBackgroundBlue,
+        teamTagBackgroundColor = KBongPrimary,
+        lottieFile = null
     );
 
     companion object {
         fun fromTypeData(teamName: String): MyTeamType {
-            return MyTeamType.entries.find { it.teamName == teamName } ?: KIA
+            return MyTeamType.entries.find { it.teamName == teamName } ?: NONE
         }
     }
 }
